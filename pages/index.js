b9070f9f07c4bd515346eab73facc87d68ef4f10
hjsr <hendyjosue@akimad.com>
2019-10-02 20:37:22 +0200

    Testing external api SSR

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,36 @@
 import JobsList from '../modules/JobsList';
+import Link from 'next/link'
 // import '../utils/style.css';
 
 // La de trabajo single
 // La de formulario que lleva al pago
 
-const Index = () => (
+const Index = (props) => (
 	<div className="container">
         <div className="hero">
             <h1>Trabajos remotos</h1>
             <p className="prata">La plataforma para encontrar trabajos online de manera sencilla.</p>
         </div>
 		<JobsList /> 
+		<h1>Batman TV Shows</h1>
+		<ul>
+			{props.shows.map(show => (
+				<li key={show.id}>
+					<Link href="/p/[id]" as={`/p/${show.id}`}>
+						<a>{show.name}</a>
+					</Link>
+				</li>
+			))}
+		</ul>
 	</div>
 );
+
+Index.getInitialProps = async function () {
+	const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+	const data = await res.json();
+	return {
+		shows: data.map(entry => entry.show)
+	};
+};
 
 export default Index;
