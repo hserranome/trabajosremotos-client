272460da3a44cee5797e8a582fda615c9c4f6966
hjsr <hendyjosue@akimad.com>
2019-10-04 19:06:12 +0200

    Init publication page

---
+++ pages/publicar.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function About() {
+	return (
+		<div>
+			<p>Somos dos losers con ganas de que la investigacion de cat girls siga adelante</p>
+
+			<Link href="/">
+				<a>Homepage</a>
+			</Link>
+		</div>
+	);
+}
