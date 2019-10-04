505ef98ae6c30c9ee63aa55ed2a47d3d54655b98
hjsr <hendyjosue@akimad.com>
2019-10-04 22:38:27 +0200

    Added layout title

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,17 @@
 import Footer from './Footer';
 import Header from './Header';
+import Head from 'next/head'
+
 import '../static/css/style.css';
 
 
 const Layout = (props) => (
     <div>
+		<Head>
+			<title>Trabajos remotos - Tu tablón de empleo remoto exclusivamente en español</title>
+			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+		</Head>
+
         <Header />
         {props.children}
         <Footer />
