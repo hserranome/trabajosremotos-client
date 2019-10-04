ce9be8a32f31ddb4da9f8cd70ec840a9d89d4bbb
hjsr <hendyjosue@akimad.com>
2019-10-04 23:00:17 +0200

    Added category page

---
+++ pages/categoria/[category].js
@@ -0,0 +1,39 @@
+import Layout from '../../components/Layout'
+import JobsList from '../../modules/JobsList';
+import fetch from 'isomorphic-unfetch';
+
+const getJobsByCategory = async (category) => {
+	const res = await fetch(`http://localhost:5000/trabajos/category/${category}`);
+	const data = await res.json();
+	return data.jobs
+}
+
+const CategoryList = (props) => {
+	const { initialJobs, error } = props;
+
+	return (
+		<Layout>
+			<div className="hero">
+				<div className="container">
+					<h1>Trabajos remotos</h1>
+					<p className="prata">La plataforma para encontrar trabajos online de manera sencilla.</p>
+				</div>
+			</div>
+
+			<div className="trabajos">
+				<JobsList initialJobs={initialJobs} error={error} />
+			</div>
+		</Layout>
+	)
+};
+
+CategoryList.getInitialProps = async ({ query }) => {
+	const { category } = query;
+	try {
+		return { initialJobs: await getJobsByCategory(category) };
+	} catch (error) {
+		return { error }
+	}
+};
+
+export default CategoryList;
