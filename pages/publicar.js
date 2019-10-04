369ff33190612355cbcb08d349fb8f44dc70391d
hjsr <hendyjosue@akimad.com>
2019-10-04 22:36:10 +0200

    removed rcform

--- pages/publicar.js
+++ pages/publicar.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import Layout from '../components/Layout'
-import { createForm } from 'rc-form';
 
 const Publicar = () => {
 	const [values, setValues] = useState({});
