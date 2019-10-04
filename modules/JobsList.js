56b389883b2531f31aba0c88494ac257b875f190
hjsr <hendyjosue@akimad.com>
2019-10-04 19:58:16 +0200

    Styled job rows
    Co-authored-by: Mascarell <echo.mascarell@gmail.com>

--- modules/JobsList.js
+++ modules/JobsList.js
@@ -3,15 +3,17 @@ import React, { useState, useEffect } from 'react';
 function JobsList (props) {
 	const { initialJobs } = props;
 	const [jobs, setJobs] = useState(initialJobs || []);
+	// Options to format the date from the database, taken from (https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date)
+	const options = { year: 'numeric', month: 'short', day: 'numeric' };
 
 	return (
 		<div className="container">
 			<div className="trabajos">
 				{jobs.map((job) => (
-					<div>
+					<div className="trabajo">
 						<a href={`/trabajo/${job.slug}`}>
-							<h3>{job.title}</h3>
-							<h4>{job.company}</h4>
+							<h2 className="prata">{job.title} <span>{new Date(job.createdAt).toLocaleDateString('es-ES', options)}</span></h2>
+							<p>{job.company}</p>
 						</a>
 					</div>
 				))}
