102c91c559b2cfd676494cb95ea94035652d97cb
hjsr <hendyjosue@akimad.com>
2019-10-05 18:44:19 +0200

    Strapi and margin

--- pages/trabajo/[slug].js
+++ pages/trabajo/[slug].js
@@ -3,42 +3,61 @@ import Head from 'next/head';
 
 function SingleJob(props) {
 	const { job } = props;
-	// Options to format the date from the database, taken from (https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date)
 	const options = { year: 'numeric', month: 'short', day: 'numeric' };
-
 	return (
 		<Layout>
 			<Head>
-				<title>{`${job.title} en ${job.company} - Trabajos remotos`}</title>
+				{job
+					? (<title>{`${job.title} en ${job.company} - Trabajos remotos`}</title>)
+					: (<title>{`404 - Trabajos remotos`}</title>)
+				}
 			</Head>
 			<div className="anuncio">
-				<div className="container">
-					<div class="content">
-						<p class="date"> {new Date(job.createdAt).toLocaleDateString('es-ES', options)} </p>
-						<h1 class="prata">{job.title}</h1>
+					{job 
+						? (
+						<div className="container">
+							<div className="content">
+								<p className="date"> {new Date(job.created_at).toLocaleDateString('es-ES', options)} </p>
+								<h1 className="prata">{job.title}</h1>
 
-						<div class="description">
-							{/* <?php the_content(); ?> <!-- Page Content --> */}
+								<div className="description">
+									{/* <?php the_content(); ?> <!-- Page Content --> */}
+								</div>
+							</div>
+
+							<div className="sidebar">
+								{/* <!-- Logo if there's any -->
+								<!-- company name -->
+								<!-- button to apply -->
+								<!-- view all jobs button --> */}
+							</div>
 						</div>
-					</div>
+						)
+						: (
+						<div className="container">
+							<div className="content">
+								<div className="empty-message">No se han encontrado este trabajo</div>
+							</div>
+						</div>
+						)
+					}
 
-					<div class="sidebar">
-						{/* <!-- Logo if there's any -->
-						<!-- company name -->
-						<!-- button to apply -->
-						<!-- view all jobs button --> */}
-					</div>
-				</div>
 			</div>
 		</Layout>
 	)
 }
 
 SingleJob.getInitialProps = async ({ query }) => {
-	const { slug } = query;
-	const res = await fetch(`http://localhost:5000/trabajo/${slug}`);
-	const job = await res.json();
-	return { job };
+	try {
+		const { slug } = query;
+		const res = await fetch(`http://localhost:1337/jobs?slug=${slug}`);
+		const jobs = await res.json();
+		const job = jobs[0];
+		return { job };
+	} catch (error) {
+		console.error(error)
+		return { error }
+	}
 };
 
 export default SingleJob;
