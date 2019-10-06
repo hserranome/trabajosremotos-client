5c9c06be420b71b1de344ddba2ecff41b0b675ab
hjsr <hendyjosue@akimad.com>
2019-10-06 03:32:03 +0200

    Init

--- pages/confirmacion.js
+++ pages/confirmacion.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { withRouter } from 'next/router'
 import axios from 'axios';
 import Head from 'next/head'
 
@@ -5,7 +7,21 @@ import Layout from '../components/Layout';
 
 
 const Confirmation = (props) => {
-	const { result } = props;
+	const { result, query } = props;
+
+	const checkJob = async () => {
+		const { session_id } = query;
+		const res = await axios.get(`http://localhost:1337/orders?session_id=${session_id}`);
+		if(res.data && res.data[0].job) {
+
+		}
+	}
+
+	// CHECK IF JOB EXISTS
+	useEffect(() => {
+		checkJob();
+	}, []);
+
 	return (
 		<Layout>
 			<Head>			
@@ -18,16 +34,8 @@ const Confirmation = (props) => {
 	)
 };
 
-Confirmation.getInitialProps = async ({ query }) => {
-	// try {
-	// 	const { session_id } = query;
-	// 	const res = await axios.post('http://localhost:1337/jobs', { session_id });
-	// 	const { data } = res;
-	// 	return { result: data };
-	// } catch (error) {
-	// 	console.log(error)
-	// 	return { error }
-	// }
-};
+Confirmation.getInitialProps = ({ query }) => {
+	return { query }
+}
 
-export default Confirmation;
+export default withRouter(Confirmation);
