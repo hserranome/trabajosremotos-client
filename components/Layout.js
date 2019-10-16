a50335099e321c6f6d7162ac969838e4036f5a76
hjsr <hendyjosue@akimad.com>
2019-10-16 20:07:31 +0200

    mailchimp

--- components/Layout.js
+++ components/Layout.js
@@ -7,15 +7,20 @@ import 'easymde/dist/easymde.min.css';
 import Header from './Header';
 import Footer from './Footer';
 
+
 // 👏 CONSISTENCIA 👏 
 const Layout = (props) => {
 	useEffect(() => {
 		if (!window.GA_INITIALIZED) {
 			// Si no esta, iniciamos
 			ReactGA.initialize('UA-108296865-5');
-			window.GA_INITIALIZED = true
+			window.GA_INITIALIZED = true;
 		}
 		ReactGA.pageview(window.location.pathname + window.location.search);
+
+		window.dojoRequire(["mojo/signup-forms/Loader"], function (L) { L.start({ "baseUrl": "mc.us4.list-manage.com", "uuid": "3a042d90b01c388c7081ca588", "lid": "591a1153bf", "uniqueMethods": true }) })
+
+
 	}, []);
 
 	return(
@@ -23,8 +28,12 @@ const Layout = (props) => {
 			<Head>
 				<title>Trabajos remotos - Tu tablón de empleo remoto exclusivamente en español</title>
 				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+				{/* Favicon */}
 				<link rel='shortcut icon' href='/static/favicon.ico' />
+				{/* Stripe */}
 				<script src='https://js.stripe.com/v3' />
+				{/* Mailchimp */}
+				<script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script>
 			</Head>
 	
 			<Header {...props} />
