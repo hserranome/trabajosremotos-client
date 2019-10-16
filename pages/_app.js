62d2a6f0e39d4c0ac20bbe4255942c30380dcce9
hjsr <hendyjosue@akimad.com>
2019-10-16 21:05:44 +0200

    progress loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import App from 'next/app';
+import Router from 'next/router';
 import fetch from 'isomorphic-unfetch';
+import NProgress from 'nprogress'
 
 import { API_URL } from '../utils';
 import Layout from '../components/Layout';
 
+Router.events.on('routeChangeStart', url => {
+	console.log(`Loading: ${url}`)
+	NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 class MyApp extends App {
 	static async getInitialProps({ Component, router, ctx }) {
