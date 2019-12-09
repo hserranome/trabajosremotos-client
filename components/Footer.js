b64ffbc81ca3b7c24016acd5b6abdcc1e7a872e9
Mascarell <echo.mascarell@gmail.com>
2019-12-09 18:35:45 +0000

    yokse

--- components/Footer.js
+++ components/Footer.js
@@ -11,6 +11,7 @@ const Footer = (props) => (
             </div>
 
             <div className="social">
+                <a href="mailto:contacto@trabajosremotos.es" className="img"><img src="/static/images/mail.svg" alt="icono twitter" /></a>
                 <a href="https://twitter.com/trabajos_remoto" target="_blank" className="img"><img src="/static/images/twitter.svg" alt="icono twitter" /></a>
             </div>
         </div>
