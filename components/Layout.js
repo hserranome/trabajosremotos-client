e40552ffa03293b7d1689355509e190c9eeba37d
hjsr <hendyjosue@akimad.com>
2019-12-09 18:36:12 +0100

    added favicons and manifest

--- components/Layout.js
+++ components/Layout.js
@@ -60,7 +60,25 @@ const Layout = (props) => {
 				<meta name="twitter:site" content="@trabajos_remoto" />
 				<meta name="twitter:creator" content="@trabajos_remoto" />
 
-				<link rel='shortcut icon' href='/static/favicon.ico' />
+				<link rel="apple-touch-icon" sizes="57x57" href="/static/favicons/apple-icon-57x57.png" />
+				<link rel="apple-touch-icon" sizes="60x60" href="/static/favicons/apple-icon-60x60.png" />
+				<link rel="apple-touch-icon" sizes="72x72" href="/static/favicons/apple-icon-72x72.png" />
+				<link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-icon-76x76.png" />
+				<link rel="apple-touch-icon" sizes="114x114" href="/static/favicons/apple-icon-114x114.png" />
+				<link rel="apple-touch-icon" sizes="120x120" href="/static/favicons/apple-icon-120x120.png" />
+				<link rel="apple-touch-icon" sizes="144x144" href="/static/favicons/apple-icon-144x144.png" />
+				<link rel="apple-touch-icon" sizes="152x152" href="/static/favicons/apple-icon-152x152.png" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-icon-180x180.png" />
+				<link rel="icon" type="image/png" sizes="192x192" href="/static/favicons/android-icon-192x192.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="96x96" href="/static/favicons/favicon-96x96.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
+				<link rel='shortcut icon' href='/static/favicons/favicon.ico' />
+				<meta name="msapplication-TileColor" content="#ffffff" />
+				<meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+				<meta name="theme-color" content="#FFFFFF" />
+
+				<link rel="manifest" href="/static/manifest.json" />
 
 				<script src='https://js.stripe.com/v3' defer />
 				{/* <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script> */}
