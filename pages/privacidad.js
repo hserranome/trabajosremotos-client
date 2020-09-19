1e4fdeb7c1ffef1d8877d345d9a73e015bcf426a
Sefhi <echo.mascarell@gmail.com>
2020-09-19 11:41:02 +0200

    paginas de privacidad y terminos

---
+++ pages/privacidad.js
@@ -0,0 +1,115 @@
+import Head from 'next/head';
+
+const Privacidad = (props) => {
+	return (
+		<div>
+			<Head>
+				<title>Trabajos remotos - Política de privacidad</title>
+
+				<meta property="og:description" content="La plataforma para encontrar trabajos remotos online de manera sencilla." />
+				<meta name="twitter:description" content="La plataforma para encontrar trabajos remotos online de manera sencilla." />
+			</Head>
+
+			<div className="anuncio">
+				<div className="container">
+					<div className="content fullwidth">
+						<h1>Política de privacidad</h1>
+						<h4>Actualizado el 19 de Septiembre de 2020</h4>
+						
+						<h4>Visión de conjunto</h4>
+
+						<p>
+							Mango Milkshake ("Mango Milkshake", "nosotros", "nos" o "nuestro") se compromete a proteger la privacidad de la información personal (es decir, cualquier información relacionada con una persona física identificada o identificable) que visite cualquiera de los sitios propiedad de Mango Milkshake y usan los servicios disponibles (los "Servicios"). Las enmiendas a esta Política de privacidad se publicarán en el Sitio y / o Servicios y entrarán en vigencia una vez publicadas. Su uso continuado del Sitio y / o los Servicios después de la publicación de cualquier enmienda a esta Política de Privacidad constituirá su aceptación de dicha enmienda.
+						</p>
+
+						<p>
+							Consentimiento y recopilación y uso de información Cuando publica un trabajo en nuestro sitio, solicitamos información personal que se utilizará para proporcionarle los servicios, comunicarse con usted sobre el estado de su anuncio y para otros fines establecidos en esta Política de privacidad. Su nombre, nombre de la compañía, dirección de correo electrónico, información de la tarjeta de crédito y cierta otra información sobre usted pueden ser requeridos por nosotros para proporcionar los Servicios o ser divulgados por usted durante el uso de los Servicios.
+						</p>
+
+						<p>
+							Al proporcionarnos información personal y al retenernos para proporcionarle los Servicios, usted acepta voluntariamente la recopilación, el uso y la divulgación de dicha información personal según lo especificado en esta Política de privacidad. Las bases legales para nuestro procesamiento de información personal son principalmente que el procesamiento es necesario para proporcionar los Servicios y que el procesamiento se lleva a cabo según nuestros intereses legítimos, que se explican con más detalle a continuación. Sin limitar lo anterior, en ocasiones podemos pedirle que consienta cuando recopilemos, usemos o divulguemos su información personal en circunstancias específicas.
+						</p>
+
+						<p>
+							Tomamos medidas diseñadas para garantizar que solo aquellos empleados que necesiten acceder a su información personal para cumplir con sus obligaciones laborales tengan acceso a ella. Podemos usar y divulgar su información personal o de cuenta para los siguientes propósitos:
+						</p>
+
+						<p>
+							Proporcionarle el Sitio y los Servicios a usted y a otros usuarios del Sitio y los Servicios; Mejorar la calidad del Sitio y los Servicios a través de encuestas, encuestas y otras actividades similares de recopilación de comentarios realizadas por Mango Milkshake y / o terceros; Para crear, administrar y controlar la información de su cuenta, y para verificar los derechos de acceso al Sitio y a los Servicios; Para facturar su cuenta; Comunicarse con usted (sujeto a sus derechos de exclusión establecidos en esta Política de privacidad), incluidos, entre otros, con el fin de proporcionarle información sobre los Servicios o informarle de cambios o adiciones a los Servicios o de la disponibilidad de cualquier otro servicio o característica que proporcionamos; Para evaluar los niveles de servicio, monitorear los patrones de tráfico y la popularidad de las diferentes características y opciones de servicio del Sitio y / o Servicios; Para hacer cumplir esta Política de Privacidad o nuestros Términos de Servicio; Para protegerse contra el fraude o error, y para responder a los reclamos de cualquier violación de nuestros derechos o los de cualquier tercero; Para responder a sus solicitudes de servicio al cliente; Para proteger los derechos, la propiedad o la seguridad personal de usted, nosotros, nuestros usuarios y el público; y Según sea necesario para cumplir con las leyes aplicables o según lo autoricen las leyes aplicables. Para efectos de facturar su cuenta (si tiene una cuenta paga) y para procesar pagos, Mango Milkshake utiliza pasarelas de pago de terceros, como Stripe, que utilizará su tarjeta de crédito y otra información de pago de acuerdo con su política de privacidad respectiva.
+						</p>
+
+						<p>
+							Cuando divulgamos su información personal a terceros, tomamos medidas razonables para garantizar el cumplimiento de las normas establecidas en esta Política de privacidad y estos terceros brindan garantías suficientes para implementar medidas técnicas y organizativas apropiadas.
+						</p>
+
+						<p>
+							Su información personal puede almacenarse y procesarse en cualquier país en el que tengamos instalaciones o en el que contratemos proveedores de servicios externos. Al utilizar los Servicios, usted acepta la transferencia de información a países fuera de su país de residencia, que pueden tener diferentes reglas de protección de datos que en su país. Si bien dicha información se encuentra fuera de España, está sujeta a las leyes del país en el que se encuentra, y puede estar sujeta a divulgación a los gobiernos, tribunales o agencias de aplicación de la ley o reguladoras de dicho otro país, de conformidad con las leyes de tal país Sin embargo, nuestras prácticas con respecto a su información personal seguirán rigiéndose en todo momento por esta Política de privacidad y, si corresponde.
+						</p>
+
+						<p>
+							Ocasionalmente podemos comunicarnos con usted con respecto a nuestros productos, servicios, noticias y eventos. Usted tiene la opción de no recibir esta información. Proporcionamos una función de exclusión en todas las comunicaciones por correo electrónico de esta naturaleza, o dejaremos de comunicarnos con usted para este fin si se comunica con nosotros y nos indica que no le comuniquemos esta información. El único tipo de estas comunicaciones para las que no puede darse de baja son las que se requieren para comunicar anuncios relacionados con los Servicios, incluida la información específica de su Cuenta, suspensiones e interrupciones planificadas de los Servicios. Intentaremos minimizar este tipo de comunicación para usted.
+						</p>
+
+						<h4>Edad de consentimiento</h4>
+
+						<p>
+							No proporcionamos a sabiendas los Servicios y no recopilamos a sabiendas la información personal de ninguna persona menor de edad. Si vive en un país de la UE / EEA, debe tener al menos 16 años para utilizar nuestros Servicios o la mayor edad requerida en su país para registrarse o utilizar nuestros Servicios. Si vive en otro país, excepto en la UE / EEA, debe tener al menos 13 años para utilizar nuestros Servicios o la mayor edad requerida en su país para registrarse o utilizar nuestros Servicios. Además de tener la edad mínima requerida para utilizar nuestros Servicios según la ley aplicable, si no tiene la edad suficiente para tener la autoridad para aceptar nuestra Política de privacidad en su país, su padre o tutor debe aceptar nuestra Política de privacidad en su nombre.
+						</p>
+
+						<p>
+							Ni el Sitio ni los Servicios están destinados a niños menores de 13 años, y ninguna persona menor de 13 años puede proporcionar información personal en, o a través del Sitio o los Servicios. No recopilamos deliberadamente información personal de niños menores de 13 años. Si tiene menos de 13 años, no use ni brinde información en, o a través del Sitio o los Servicios, realice compras a través del Sitio o los Servicios, utilice cualquiera de los servicios interactivos o características de comentarios públicos, o brinde información sobre usted, incluidos su nombre, dirección, número de teléfono, dirección de correo electrónico o cualquier nombre de pantalla o nombre de usuario que pueda usar. Si descubrimos que hemos recopilado o recibido información personal de un niño menor de 13 años sin verificación del consentimiento de los padres, eliminaremos dicha información. Si cree que podemos tener alguna información de o sobre un niño menor de 13 años.
+						</p>
+
+						<h4>Derechos a su información</h4>
+
+						<p>
+							Bajo el GDPR, usted puede tener derecho a derechos adicionales, que incluyen: (i) el derecho a retirar el consentimiento para el procesamiento, donde el consentimiento es la base del procesamiento; (ii) el derecho de acceder a su información personal y cierta otra información suplementaria, bajo ciertas condiciones; (iii) el derecho a oponerse al procesamiento de datos ilícitos, bajo ciertas condiciones; (iv) el derecho a borrar la información personal sobre usted, bajo ciertas condiciones; (v) el derecho a exigir que restrinjamos el procesamiento de su información personal, bajo ciertas condiciones, si cree que hemos excedido la base legítima para el procesamiento, el procesamiento ya no es necesario, está procesando o creemos que su información personal es inexacta; (vi) el derecho a la portabilidad de los datos de su información personal que nos haya proporcionado de forma estructurada, de uso común, y formato legible por máquina, bajo ciertas condiciones; (vii) el objeto correcto para las decisiones tomadas por medios automáticos que producen efectos legales sobre usted o que de manera similar lo afectan significativamente, bajo ciertas condiciones; (viii) el derecho a presentar una queja ante las autoridades de protección de datos. Si desea obtener más información sobre sus derechos en virtud del GDPR, puede visitar la página de la Comisión Europea sobre protección de datos.
+						</p>
+
+						<h4>Datos agregados</h4>
+
+						<p>
+							También podemos usar su información personal para generar datos agregados para uso interno y para compartir con otros de manera selectiva. "Datos agregados" significa los registros que han sido despojados de información que potencialmente identifica a los usuarios, y que han sido manipulados o combinados para proporcionar información anónima generalizada. Su identidad e información personal se mantendrán anónimas en Datos agregados.
+						</p>
+
+						<h4>Anuncios de terceros y basados ​​en intereses</h4>
+
+						<p>No controlamos la recopilación o el uso de su información por parte de terceros para publicar anuncios basados ​​en intereses. Sin embargo, estos terceros pueden brindarle formas de elegir no recopilar o utilizar su información de esta manera. Puede optar por no recibir anuncios orientados de los miembros de la Iniciativa de publicidad en la red ("NAI") en el sitio web de NAI.</p>
+
+						<h4>Seguridad y Retención</h4>
+
+						<p>
+							Nos esforzaremos para evitar el acceso no autorizado a su información personal, sin embargo, no se garantiza que la transmisión de datos a través de Internet, por dispositivo inalámbrico o por aire sea 100% segura. Hemos implementado y mantenemos procedimientos y prácticas de seguridad razonables (basados ​​en la naturaleza de la información que recopilamos) para proteger esa información de la divulgación no autorizada. Continuaremos mejorando los procedimientos de seguridad a medida que nuevas tecnologías y procedimientos estén disponibles.
+						</p>
+
+						<p>
+							Le recomendamos encarecidamente que no revele su contraseña a nadie.
+						</p>
+
+						<p>
+							Recuerde que controla qué información personal proporciona al utilizar el Sitio y los Servicios. En última instancia, usted es responsable de mantener el secreto de su identificación, contraseñas y / o cualquier información personal en su poder para el uso del Sitio y / o los Servicios. Siempre sea cuidadoso y responsable con respecto a su información personal. No somos responsables, y no podemos controlar, el uso por parte de terceros de la información que les brinde y debe tener precaución al seleccionar la información personal que proporciona a otros a través del Sitio o los Servicios. Del mismo modo, no podemos asumir ninguna responsabilidad por el contenido de cualquier información personal u otra información que reciba de otros usuarios a través del Sitio o los Servicios, y nos libera de cualquier y toda responsabilidad en relación con el contenido de cualquier información personal u otra información que pueda recibir utilizando el Sitio o los Servicios. No podemos garantizar ni asumir ninguna responsabilidad para verificar la exactitud de la información personal u otra información proporcionada por un tercero. Usted nos libera de cualquier responsabilidad en relación con el uso de dicha información personal u otra información de terceros.
+						</p>
+
+						<p>
+							Mantendremos su información personal por el tiempo que sea necesario o según lo exijan las leyes, regulaciones u órdenes gubernamentales vigentes.
+						</p>
+
+						<h4>Cambios a esta política</h4>
+
+						<p>
+							Podemos actualizar esta Política de privacidad de vez en cuando. Si lo hacemos, enviaremos un correo electrónico a los usuarios suscritos a la lista de noticias de la compañía. También agregaremos un banner de sitio para alertar a los usuarios que no estén suscritos. Si el cambio afecta materialmente el tratamiento de sus datos personales, y tenemos su correo electrónico, pero usted no está suscrito a la lista de Noticias de la Compañía, le enviaremos un correo electrónico. (Usted es responsable de asegurarse de tener un correo electrónico actualizado para este fin).
+						</p>
+
+						<h4>Contáctenos</h4>
+
+						<p>
+							Si tiene alguna pregunta o comentario sobre esta Política de privacidad o su información personal, para realizar una solicitud de acceso o corrección, ejercer los derechos aplicables, presentar una queja u obtener información sobre nuestras políticas y prácticas con respecto a cualquier proveedor de servicios Fuera de España, nuestro Oficial de Privacidad (o el Oficial de Protección de Datos) puede ser contactado por correo o correo electrónico utilizando la siguiente información de contacto: por correo electrónico a contacto@trabajosremotos.es.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+};
+
+export default Privacidad;
