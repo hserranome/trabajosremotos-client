2071e8b472cce3ddf19eb835a8e2f2009bd5bc2d
Sefhi <echo.mascarell@gmail.com>
2021-01-06 11:19:09 +0100

    fixed links on footer and categories menu

--- components/CategoryMenu.js
+++ components/CategoryMenu.js
@@ -1,81 +1,43 @@
 import React from 'react';
-import Link from './ActiveLink';
+import { WEB_URL } from '../utils';
 
 const CategoryMenu = () => (
-  <div className="container">
-    <div className="categories">
-      <Link
-        href='/categoria/[category]'
-        as='/categoria/programacion'
-      >
-        <a>
-					<img src='/static/images/programacion.svg' alt='icono de programacion' />
-          <span>Programación</span>
-        </a>
-      </Link>
-      <Link
-        href='/categoria/[category]'
-        as='/categoria/diseno'
-      >
-        <a>
-					<img src='/static/images/diseno.svg' alt='icono de diseno' />
-          <span>Diseño</span>
-        </a>
-      </Link>
-      <Link
-        href='/categoria/[category]'
-        as='/categoria/marketing'
-      >
-        <a>
-					<img src='/static/images/marketing.svg' alt='icono de marketing' />
-          <span>Marketing</span>
-        </a>
-      </Link>
-      <Link
-        href='/categoria/[category]'
-        as='/categoria/atencion-al-cliente'
-      >
-        <a>
-					<img src='/static/images/atencion.svg' alt='icono de atencion' />
-          <span>Atención <br/> al cliente</span>
-        </a>
-      </Link>
-      <Link
-        href='/categoria/[category]'
-        as='/categoria/gamedev'
-      >
-        <a>
-					<img src='/static/images/gamedev.svg' alt='icono de gamedev' />
-          <span>GameDev</span>
-        </a>
-      </Link>
-      <Link
-        href='/categoria/[category]'
-        as='/categoria/otros'
-      >
-        <a>
-					<img src='/static/images/otros.svg' alt='icono de otros' />
-          <span>Otros</span>
-        </a>
-      </Link>
-      <Link
-        href='https://t.me/trabajos_remotos'
-      >
-        <a>
-					<img src='/static/images/telegram.svg' alt='icono de telegram' />
-          <span>Grupo de <br/> Telegram</span>
-        </a>
-      </Link>
-      <Link
-		href='https://notionmango.com/'
-      >
-        <a>
-					<img src='/static/images/notion.svg' alt='icono de notion' />
-          <span>Plantillas de <br/> Notion</span>
-        </a>
-      </Link>
-      <div style={{marginLeft: '2rem'}}></div>
-    </div>
-  </div>
+	<div className="container">
+		<div className="categories">
+			<a href={`${WEB_URL}categoria/programacion`} >
+				<img src='/static/images/programacion.svg' alt='icono de programacion' />
+				<span>Programación</span>
+			</a>
+			<a href={`${WEB_URL}categoria/diseno`}>
+				<img src='/static/images/diseno.svg' alt='icono de diseno' />
+				<span>Diseño</span>
+			</a>
+			<a href={`${WEB_URL}categoria/marketing`}>
+				<img src='/static/images/marketing.svg' alt='icono de marketing' />
+				<span>Marketing</span>
+			</a>
+			<a href={`${WEB_URL}categoria/atencion-al-cliente`}>
+				<img src='/static/images/atencion.svg' alt='icono de atencion' />
+				<span>Atención <br /> al cliente</span>
+			</a>
+			<a href={`${WEB_URL}categoria/gamedev`}>
+				<img src='/static/images/gamedev.svg' alt='icono de gamedev' />
+				<span>GameDev</span>
+			</a>
+			<a href={`${WEB_URL}categoria/otros`}>
+				<img src='/static/images/otros.svg' alt='icono de otros' />
+				<span>Otros</span>
+			</a>
+			<a href='https://t.me/trabajos_remotos'>
+				<img src='/static/images/telegram.svg' alt='icono de telegram' />
+				<span>Grupo de <br /> Telegram</span>
+			</a>
+			<a href='https://notionmango.com/'>
+				<img src='/static/images/notion.svg' alt='icono de notion' />
+				<span>Plantillas de <br /> Notion</span>
+			</a>
+			<div style={{ marginLeft: '2rem' }}></div>
+		</div>
+	</div>
 )
 export default CategoryMenu;
--- components/Footer.js
+++ components/Footer.js
@@ -1,71 +1,72 @@
 import Link from './ActiveLink';
 import LazyLoad from 'react-lazyload';
+import { WEB_URL } from '../utils';
 
 const Footer = (props) => (
-    <div className="footer">
-      <div className="container">
-        <div className="copy big">
-					<div className="inside">
-						<h4>Trabajos remotos</h4>
+	<div className="footer">
+		<div className="container">
+			<div className="copy big">
+				<div className="inside">
+					<h4>Trabajos remotos</h4>
 
-						<p>Somos la comunidad hispanohablante más grande para encontrar empleo remoto. 🔥</p>
-					</div>
-				</div>
-
-				{/* Empty copy to have more space */}
-				<div className="copy small"></div>
-			
-				<div className="copy">
-					<div className="inside">
-						<h4>Empleos remotos</h4>
-
-						<p><Link href="/categoria/programacion"><a>Trabajos de programación</a></Link></p>
-						<p><Link href="/categoria/diseno"><a>Trabajos de diseño</a></Link></p>
-						<p><Link href="/categoria/marketing"><a>Trabajos de marketing</a></Link></p>
-						<p><Link href="/categoria/atencion-al-cliente"><a>Trabajos de atención al cliente</a></Link></p>
-						<p><Link href="/categoria/otros"><a>Otros trabajos</a></Link></p>
-					</div>
-				</div>
-			
-				<div className="copy">
-					<div className="inside">
-						<h4>Sobre nosotros</h4>
-
-						<p><Link href="/contacto"><a>Contacto</a></Link></p>
-						<p><Link href="/recursos"><a>Recursos</a></Link></p>
-						<p><Link href="/publicar"><a>Publicar anuncio</a></Link></p>
-						<p><Link href="/cookies"><a>Cookies</a></Link></p>
-						<p><Link href="/privacidad"><a>Privacidad</a></Link></p>
-						<p><Link href="/terminos"><a>Terminos y condiciones</a></Link></p>
-					</div>
+					<p>Somos la comunidad hispanohablante más grande para encontrar empleo remoto. 🔥</p>
 				</div>
 			</div>
 
-			<div className="container copyright">
-				<p>&copy; 2020 <a style={{ marginLeft: 0 }} rel="noopener" target="_blank" href="https://www.mango.moe?ref=trabajos">MangoMoe</a></p>
+			{/* Empty copy to have more space */}
+			<div className="copy small"></div>
 
-				<div>
-					{/* Social media buttons */}
-					<div className="img">
-						<a rel="noopener" target="_blank" href="https://www.linkedin.com/company/trabajosremotos">
-							<img src='/static/images/linkedin.svg' alt='icono de linkedin' />
-						</a>
-					</div>
+			<div className="copy">
+				<div className="inside">
+					<h4>Empleos remotos</h4>
 
-					<div className="img">
-						<a rel="noopener" target="_blank" href="https://www.instagram.com/trabajos_remoto/">
-							<img src='/static/images/instagram.svg' alt='icono de instagram' />
-						</a>
-					</div>
-
-					<div className="img">
-						<a rel="noopener" target="_blank" href="https://t.me/trabajos_remotos">
-							<img src='/static/images/telegram2.svg' alt='icono de telegram' />
-						</a>
-					</div>
+					<p><a href={`${WEB_URL}/categoria/programacion`}>Trabajos de programación</a></p>
+					<p><a href={`${WEB_URL}/categoria/diseno`}>Trabajos de diseño</a></p>
+					<p><a href={`${WEB_URL}/categoria/marketing`}>Trabajos de marketing</a></p>
+					<p><a href={`${WEB_URL}/categoria/atencion-al-cliente`}>Trabajos de atención al cliente</a></p>
+					<p><a href={`${WEB_URL}/categoria/otros`}>Otros trabajos</a></p>
 				</div>
 			</div>
-    </div>
+
+			<div className="copy">
+				<div className="inside">
+					<h4>Sobre nosotros</h4>
+
+					<p><a href={`${WEB_URL}/contacto`}>Contacto</a></p>
+					<p><a href={`${WEB_URL}/recursos`}>Recursos</a></p>
+					<p><a href={`${WEB_URL}/publicar`}>Publicar anuncio</a></p>
+					<p><a href={`${WEB_URL}/cookies`}>Cookies</a></p>
+					<p><a href={`${WEB_URL}/privacidad`}>Privacidad</a></p>
+					<p><a href={`${WEB_URL}/terminos`}>Terminos y condiciones</a></p>
+				</div>
+			</div>
+		</div>
+
+		<div className="container copyright">
+			<p>&copy; 2020 <a style={{ marginLeft: 0 }} rel="noopener" target="_blank" href="https://www.mango.moe?ref=trabajos">MangoMoe</a></p>
+
+			<div>
+				{/* Social media buttons */}
+				<div className="img">
+					<a rel="noopener" target="_blank" href="https://www.linkedin.com/company/trabajosremotos">
+						<img src='/static/images/linkedin.svg' alt='icono de linkedin' />
+					</a>
+				</div>
+
+				<div className="img">
+					<a rel="noopener" target="_blank" href="https://www.instagram.com/trabajos_remoto/">
+						<img src='/static/images/instagram.svg' alt='icono de instagram' />
+					</a>
+				</div>
+
+				<div className="img">
+					<a rel="noopener" target="_blank" href="https://t.me/trabajos_remotos">
+						<img src='/static/images/telegram2.svg' alt='icono de telegram' />
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
 )
 
 export default Footer;
