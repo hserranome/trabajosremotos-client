fd9a3506c8be3e94bd0b6f6bd128bb3c0c3b4d23
Hendy Serrano <hello@hserrano.me>
2021-09-27 22:13:55 +0200

    remove scrollytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,28 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from 'styled-components'
+import { ServerStyleSheet } from "styled-components";
 
-import * as Sentry from '@sentry/browser';
+import * as Sentry from "@sentry/browser";
 
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
 	Sentry.captureException(err);
 });
 
-process.on('uncaughtException', (err) => {
+process.on("uncaughtException", (err) => {
 	Sentry.captureException(err);
 });
 
 export default class MyDocument extends Document {
 	static async getInitialProps(ctx) {
-		const sheet = new ServerStyleSheet()
-		const originalRenderPage = ctx.renderPage
+		const sheet = new ServerStyleSheet();
+		const originalRenderPage = ctx.renderPage;
 
 		try {
 			ctx.renderPage = () =>
 				originalRenderPage({
-					enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-				})
+					enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+				});
 
-			const initialProps = await Document.getInitialProps(ctx)
+			const initialProps = await Document.getInitialProps(ctx);
 			return {
 				...initialProps,
 				styles: (
@@ -30,19 +30,25 @@ export default class MyDocument extends Document {
 						{initialProps.styles}
 						{sheet.getStyleElement()}
 					</>
-				)
-			}
+				),
+			};
 		} finally {
-			sheet.seal()
+			sheet.seal();
 		}
 	}
 	render() {
 		return (
 			<Html lang="es">
-			<Head>
-					{process.env.NODE_ENV == 'production' ? <script async defer data-website-id="9b2e0d69-64ef-40d9-917f-6e6a1d2f889c" src="https://analytics.mango.moe/umami.js"></script> : null}
-					{process.env.NODE_ENV == 'production' ? <script src="https://scrollytics.com/scrollytics.js" data-pid="ZUYgTyKyoDsj"></script> : null}
-			</Head>
+				<Head>
+					{process.env.NODE_ENV == "production" ? (
+						<script
+							async
+							defer
+							data-website-id="9b2e0d69-64ef-40d9-917f-6e6a1d2f889c"
+							src="https://analytics.mango.moe/umami.js"
+						></script>
+					) : null}
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
