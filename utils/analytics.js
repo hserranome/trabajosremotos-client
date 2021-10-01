188fb3fe77e6088b68566e74938664ed9201a073
Hendy Serrano <hendyjosue@gmail.com>
2021-10-01 22:57:49 +0200

    removed click on job and removed job expired check

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,6 +1,5 @@
 const eventTypes = {
 	"apply-to-job": "apply-to-job",
-	"click-on-job": "click-on-job",
 	"click-on-banner": "click-on-banner",
 	"click-on-cta": "click-on-cta",
 	"click-on-analytics": "click-on-analytics",
