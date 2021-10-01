d571f38836304789e10214e960beea7ba39b2b69
Hendy Serrano <hello@hserrano.me>
2021-10-01 13:56:37 +0200

    publicitate page

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import Link from './ActiveLink';
-import { WEB_URL } from '../utils';
+import React, { useEffect, useState } from "react";
+import Link from "./ActiveLink";
+import { WEB_URL } from "../utils";
 
 class Header extends React.Component {
 	constructor(props) {
@@ -12,107 +12,113 @@ class Header extends React.Component {
 	}
 
 	componentDidMount() {
-		window.addEventListener('scroll', this.handleScroll);
+		window.addEventListener("scroll", this.handleScroll);
 	}
 
 	componentWillUnmount() {
-		window.removeEventListener('scroll', this.handleScroll);
+		window.removeEventListener("scroll", this.handleScroll);
 	}
 
 	handleScroll(event) {
-		let scrollY = window.scrollY
-		let menuDesktop = document.querySelector('nav.desktop')
+		let scrollY = window.scrollY;
+		let menuDesktop = document.querySelector("nav.desktop");
 
-		if(scrollY > 0)
-			menuDesktop.classList.add('active')
-		else
-			menuDesktop.classList.remove('active')
+		if (scrollY > 0) menuDesktop.classList.add("active");
+		else menuDesktop.classList.remove("active");
 	}
-	
+
 	handleMobileMenu() {
-		let menu = document.querySelector('.b-container');
-		let overlay = document.querySelector('.overlay');
+		let menu = document.querySelector(".b-container");
+		let overlay = document.querySelector(".overlay");
 
 		// Open and close the menu
-		menu.classList.toggle('open');
+		menu.classList.toggle("open");
 
-		if (menu.classList.contains('open')) {
+		if (menu.classList.contains("open")) {
 			// When the menu is open, disable scrolling
-			document.body.style.overflow = 'hidden';
+			document.body.style.overflow = "hidden";
 			window.scrollTo(0, 0);
-			overlay.classList.add('open');
+			overlay.classList.add("open");
 		} else {
 			// When the menu is closed, we enable again the scroll on the site
-			document.body.style.overflow = 'visible';
-			overlay.classList.remove('open');
+			document.body.style.overflow = "visible";
+			overlay.classList.remove("open");
 		}
 	}
 
 	closeMenu() {
-		let overlay = document.querySelector('.overlay');
-		let menu = document.querySelector('.b-container');
+		let overlay = document.querySelector(".overlay");
+		let menu = document.querySelector(".b-container");
 
-		document.body.style.overflow = 'visible';
+		document.body.style.overflow = "visible";
 
 		try {
 			// close the menu
-			menu.classList.remove('open');
-			overlay.classList.remove('open');
+			menu.classList.remove("open");
+			overlay.classList.remove("open");
 		} catch (error) {
-			console.error('cant close the menu')
+			console.error("cant close the menu");
 		}
 	}
 
 	openSubmenu(submenu) {
 		try {
-			let sub = [...document.querySelectorAll(`.${submenu}`)]
+			let sub = [...document.querySelectorAll(`.${submenu}`)];
 
-			sub.forEach(element => {
-				element.classList.toggle('open')
-			})
+			sub.forEach((element) => {
+				element.classList.toggle("open");
+			});
 		} catch (error) {
-			console.error(error)
+			console.error(error);
 		}
 	}
 
 	render() {
 		return (
 			<div>
-				<nav className='desktop'>
-					<div className='container'>
-						<Link href='/'>
-							<div className='logo'><img src='/static/images/logo-big.jpg' /></div>
+				<nav className="desktop">
+					<div className="container">
+						<Link href="/">
+							<div className="logo">
+								<img src="/static/images/logo-big.jpg" />
+							</div>
 						</Link>
 
 						<ul>
 							<li className="dropdown-parent">
-							<a>Comunidad</a>
-							<ul className="dropdown">
-								<li>
-									<Link href='/recursos'>
-										<a>Recursos</a>
-									</Link>
-								</li>
-								<li>
-									<Link href=''>
-										<a href="https://emailoctopus.com/lists/347308e1-960d-11ea-a3d0-06b4694bee2a/forms/subscribe" target="_blank" rel="noopener">Newsletter</a>
-									</Link>
-								</li>
-							</ul>
-						</li>
+								<a>Comunidad</a>
+								<ul className="dropdown">
+									<li>
+										<Link href="/recursos">
+											<a>Recursos</a>
+										</Link>
+									</li>
+									<li>
+										<Link href="">
+											<a
+												href="https://emailoctopus.com/lists/347308e1-960d-11ea-a3d0-06b4694bee2a/forms/subscribe"
+												target="_blank"
+												rel="noopener"
+											>
+												Newsletter
+											</a>
+										</Link>
+									</li>
+								</ul>
+							</li>
 							<li>
-								<Link href='/blog'>
+								<Link href="/blog">
 									<a>Blog</a>
 								</Link>
 							</li>
 							<li>
-								<Link href='/contacto'>
-									<a>Contacto</a>
+								<Link href="/publicitate">
+									<a>Publicítate</a>
 								</Link>
 							</li>
 							<li>
-								<Link href='/publicar'>
-									<a className='main-button'>Publicar trabajo</a>
+								<Link href="/publicar">
+									<a className="main-button">Publicar trabajo</a>
 								</Link>
 							</li>
 						</ul>
@@ -123,23 +129,29 @@ class Header extends React.Component {
 				<div className="overlay">
 					<div className="links">
 						<li onClick={this.closeMenu}>
-							<Link href='/'>
+							<Link href="/">
 								<a>Trabajos remotos</a>
 							</Link>
 						</li>
 						<li onClick={this.closeMenu}>
-							<Link href='/blog'>
+							<Link href="/blog">
 								<a>Blog</a>
 							</Link>
 						</li>
 						<li>
-							<Link href=''>
-								<a href="https://emailoctopus.com/lists/347308e1-960d-11ea-a3d0-06b4694bee2a/forms/subscribe" target="_blank" rel="noopener">Newsletter</a>
+							<Link href="">
+								<a
+									href="https://emailoctopus.com/lists/347308e1-960d-11ea-a3d0-06b4694bee2a/forms/subscribe"
+									target="_blank"
+									rel="noopener"
+								>
+									Newsletter
+								</a>
 							</Link>
 						</li>
 						<li>
 							<div className="submenu-container submenu-categories">
-								<a onClick={() => this.openSubmenu('submenu-categories')}>Categorias</a>
+								<a onClick={() => this.openSubmenu("submenu-categories")}>Categorias</a>
 
 								<div className="plusButton">
 									<span></span>
@@ -148,40 +160,28 @@ class Header extends React.Component {
 							</div>
 							<ul className="submenu submenu-categories">
 								<li>
-									<a href={`${WEB_URL}/categoria/programacion`} >
-										Programación
-									</a>
+									<a href={`${WEB_URL}/categoria/programacion`}>Programación</a>
 								</li>
 								<li>
-									<a href={`${WEB_URL}/categoria/diseno`}>
-										Diseño
-									</a>
+									<a href={`${WEB_URL}/categoria/diseno`}>Diseño</a>
 								</li>
 								<li>
-									<a href={`${WEB_URL}/categoria/marketing`}>
-										Marketing
-									</a>
+									<a href={`${WEB_URL}/categoria/marketing`}>Marketing</a>
 								</li>
 								<li>
-									<a href={`${WEB_URL}/categoria/atencion-al-cliente`}>
-										Atención al cliente
-									</a>
+									<a href={`${WEB_URL}/categoria/atencion-al-cliente`}>Atención al cliente</a>
 								</li>
 								<li>
-									<a href={`${WEB_URL}/categoria/gamedev`}>
-										GameDev
-									</a>
+									<a href={`${WEB_URL}/categoria/gamedev`}>GameDev</a>
 								</li>
 								<li>
-									<a href={`${WEB_URL}/categoria/otros`}>
-										Otros
-									</a>
+									<a href={`${WEB_URL}/categoria/otros`}>Otros</a>
 								</li>
 							</ul>
 						</li>
 						<li>
 							<div className="submenu-container submenu-social-media">
-								<a onClick={() => this.openSubmenu('submenu-social-media')}>Redes sociales</a>
+								<a onClick={() => this.openSubmenu("submenu-social-media")}>Redes sociales</a>
 
 								<div className="plusButton">
 									<span></span>
@@ -190,35 +190,29 @@ class Header extends React.Component {
 							</div>
 							<ul className="submenu submenu-social-media">
 								<li>
-									<a href={`https://t.me/trabajos_remotos`} >
-										Telegram
-									</a>
+									<a href={`https://t.me/trabajos_remotos`}>Telegram</a>
 								</li>
 								<li>
-									<a href={`https://www.linkedin.com/company/trabajosremotos`}>
-										LinkedIn
-									</a>
+									<a href={`https://www.linkedin.com/company/trabajosremotos`}>LinkedIn</a>
 								</li>
 								<li>
-									<a href={`https://www.instagram.com/trabajos_remoto/`}>
-										Instagram
-									</a>
+									<a href={`https://www.instagram.com/trabajos_remoto/`}>Instagram</a>
 								</li>
 							</ul>
 						</li>
 						<li onClick={this.closeMenu}>
-							<Link href='/recursos'>
+							<Link href="/recursos">
 								<a>Recursos</a>
 							</Link>
 						</li>
 						<li onClick={this.closeMenu}>
-							<Link href='/contacto'>
-								<a>Contacto</a>
+							<Link href="/publicitate">
+								<a>Publicítate</a>
 							</Link>
 						</li>
 						<li onClick={this.closeMenu}>
-							<Link href='/publicar'>
-								<a className='main-button'>Publicar trabajo</a>
+							<Link href="/publicar">
+								<a className="main-button">Publicar trabajo</a>
 							</Link>
 						</li>
 					</div>
@@ -227,8 +221,10 @@ class Header extends React.Component {
 				<div className="mobile">
 					<div className="nav">
 						{/* Logo */}
-						<Link href='/'>
-							<div onClick={this.closeMenu} className='logo'><img src='/static/images/logo-big.jpg' /></div>
+						<Link href="/">
+							<div onClick={this.closeMenu} className="logo">
+								<img src="/static/images/logo-big.jpg" />
+							</div>
 						</Link>
 
 						{/* Burger menu for mobile */}
---
+++ pages/publicitate.js
@@ -0,0 +1,327 @@
+import Head from "next/head";
+import { FacebookChat } from "../components/FacebookChat";
+
+const Publicitate = () => {
+	return (
+		<div>
+			<Head>
+				<title>Publicar nuevo trabajo - Trabajos remotos</title>
+
+				<meta
+					property="og:description"
+					content="La plataforma para encontrar trabajos remotos online de manera sencilla."
+				/>
+				<meta
+					name="twitter:description"
+					content="La plataforma para encontrar trabajos remotos online de manera sencilla."
+				/>
+			</Head>
+			<FacebookChat />
+			<div className="pt-20 pb-32">
+				<Content />
+				<Pricing />
+			</div>
+		</div>
+	);
+};
+export default Publicitate;
+
+export const Content = () => {
+	return (
+		<div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+			<div className="grid gap-12 row-gap-8 lg:grid-cols-2">
+				<div className="flex flex-col justify-center">
+					<div className="max-w-xl mb-6">
+						<h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+							Publicitate con trabajosremotos.es
+						</h2>
+						<p className="text-base text-gray-700 md:text-lg">
+							Ofrecemos la oportunidad de anunciarte en nuestros canales a un publico hispanoablante interesado en
+							avanzar su carrera profesional.
+						</p>
+					</div>
+					<div className="grid gap-8 row-gap-8 sm:grid-cols-1">
+						<div>
+							<div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full brand-secondary-bg-color">
+								<svg className="w-8 h-8 text-deep-purple-accent-300" stroke="currentColor" viewBox="0 0 52 52">
+									<polygon
+										strokeWidth="3"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										fill="none"
+										points="29 13 14 29 25 29 23 39 38 23 27 23"
+									/>
+								</svg>
+							</div>
+							<h6 className="mb-2 font-semibold leading-5">Nuestra audiencia</h6>
+							<div class="pb-4 px-4 w-full flex flex-col text-gray-700 ">
+								<p>
+									<span class="brand-secondary-bg-color text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+										<svg
+											fill="none"
+											stroke="currentColor"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="3"
+											class="w-3 h-3"
+											viewBox="0 0 24 24"
+										>
+											<path d="M20 6L9 17l-5-5"></path>
+										</svg>
+									</span>
+									Publico hispanohablante, de <b className="font-medium">entre 25 y 45 años</b> de edad.
+								</p>
+								<p>
+									<span class="brand-secondary-bg-color text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+										<svg
+											fill="none"
+											stroke="currentColor"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="3"
+											class="w-3 h-3"
+											viewBox="0 0 24 24"
+										>
+											<path d="M20 6L9 17l-5-5"></path>
+										</svg>
+									</span>
+									La mayoria residen en <b className="font-medium">Colombia 🇨🇴, México 🇲🇽, y EE. UU 🇺🇸</b>.
+								</p>
+								<p>
+									<span class="brand-secondary-bg-color text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+										<svg
+											fill="none"
+											stroke="currentColor"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="3"
+											class="w-3 h-3"
+											viewBox="0 0 24 24"
+										>
+											<path d="M20 6L9 17l-5-5"></path>
+										</svg>
+									</span>
+									<b className="font-medium">45.000 visitas</b> de 10.000 visitantes únicos cada mes
+								</p>
+								<p>
+									<span class="brand-secondary-bg-color text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+										<svg
+											fill="none"
+											stroke="currentColor"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="3"
+											class="w-3 h-3"
+											viewBox="0 0 24 24"
+										>
+											<path d="M20 6L9 17l-5-5"></path>
+										</svg>
+									</span>
+									Nuestro canal de Telegram tiene <b className="font-medium">1000 usuarios</b> activos
+								</p>
+								<p>
+									<span class="brand-secondary-bg-color text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+										<svg
+											fill="none"
+											stroke="currentColor"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="3"
+											class="w-3 h-3"
+											viewBox="0 0 24 24"
+										>
+											<path d="M20 6L9 17l-5-5"></path>
+										</svg>
+									</span>
+									Nuestra página de LinkedIn tiene más de <b className="font-medium">1200 seguidores</b>
+								</p>
+								<p>
+									<span class="brand-secondary-bg-color text-indigo-400 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+										<svg
+											fill="none"
+											stroke="currentColor"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="3"
+											class="w-3 h-3"
+											viewBox="0 0 24 24"
+										>
+											<path d="M20 6L9 17l-5-5"></path>
+										</svg>
+									</span>
+									En nuestro Instagram nos siguen más de <b className="font-medium">850 personas</b>.{" "}
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="flex items-center justify-center -mx-4 lg:pl-8">
+					<div className="flex flex-col items-end px-3">
+						<img
+							className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
+							src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+							alt=""
+						/>
+						<img
+							className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
+							src="https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+							alt=""
+						/>
+					</div>
+					<div className="px-3">
+						<img
+							className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
+							src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+							alt=""
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export const Pricing = () => {
+	return (
+		<div className="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+			<div className="max-w-xl mb-1 md:mx-auto sm:text-center">
+				<p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+					Paquetes
+				</p>
+			</div>
+			<div className="grid max-w-md gap-10 row-gap-5 sm:row-gap-10 lg:max-w-screen-md lg:grid-cols-1 sm:mx-auto">
+				<div className="flex flex-col justify-between p-5 bg-white border rounded shadow-sm">
+					<div className="mb-6">
+						<div className="flex items-center justify-between pb-6 mb-6 border-b">
+							<div>
+								<p className="text-sm font-bold tracking-wider uppercase">Paquete estandar</p>
+								<p className="text-5xl font-extrabold">150€/mes</p>
+							</div>
+							<div className="flex items-center justify-center w-24 h-24 rounded-full bg-blue-gray-50">
+								<svg className="w-10 h-10 text-gray-600" viewBox="0 0 24 24" strokeLinecap="round" strokeLidth="2">
+									<path
+										d="M12,7L12,7 c-1.657,0-3-1.343-3-3v0c0-1.657,1.343-3,3-3h0c1.657,0,3,1.343,3,3v0C15,5.657,13.657,7,12,7z"
+										fill="none"
+										stroke="currentColor"
+									/>
+									<path
+										d="M15,23H9v-5H7v-6 c0-1.105,0.895-2,2-2h6c1.105,0,2,0.895,2,2v6h-2V23z"
+										fill="none"
+										stroke="currentColor"
+									/>
+								</svg>
+							</div>
+						</div>
+						<div>
+							<p className="mb-2 font-bold tracking-wide">Que incluye</p>
+							<ul className="space-y-2">
+								<li className="flex items-center">
+									<div className="mr-2">
+										<svg
+											className="w-4 h-4 text-deep-purple-accent-400"
+											viewBox="0 0 24 24"
+											strokeLinecap="round"
+											strokeLidth="2"
+										>
+											<polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8" />
+											<circle cx="12" cy="12" fill="none" r="11" stroke="currentColor" />
+										</svg>
+									</div>
+									<p className="font-medium text-gray-800">
+										Instagram Story con enlace, la cual destacaremos todo el mes en nuestro perfil
+									</p>
+								</li>
+								<li className="flex items-center">
+									<div className="mr-2">
+										<svg
+											className="w-4 h-4 text-deep-purple-accent-400"
+											viewBox="0 0 24 24"
+											strokeLinecap="round"
+											strokeLidth="2"
+										>
+											<polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8" />
+											<circle cx="12" cy="12" fill="none" r="11" stroke="currentColor" />
+										</svg>
+									</div>
+									<p className="font-medium text-gray-800">Anuncio nativo en la barra de categorías</p>
+								</li>
+								<li className="flex items-center">
+									<div className="mr-2">
+										<svg
+											className="w-4 h-4 text-deep-purple-accent-400"
+											viewBox="0 0 24 24"
+											strokeLinecap="round"
+											strokeLidth="2"
+										>
+											<polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8" />
+											<circle cx="12" cy="12" fill="none" r="11" stroke="currentColor" />
+										</svg>
+									</div>
+									<p className="font-medium text-gray-800">
+										Anuncio en el listado de trabajos, con título, descripción y CTA con enlace
+									</p>
+								</li>
+								<li className="flex items-center">
+									<div className="mr-2">
+										<svg
+											className="w-4 h-4 text-deep-purple-accent-400"
+											viewBox="0 0 24 24"
+											strokeLinecap="round"
+											strokeLidth="2"
+										>
+											<polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8" />
+											<circle cx="12" cy="12" fill="none" r="11" stroke="currentColor" />
+										</svg>
+									</div>
+									<p className="font-medium text-gray-800">Anuncio en nuestra newsletter semanal durante 4 semanas</p>
+								</li>
+								<li className="flex items-center">
+									<div className="mr-2">
+										<svg
+											className="w-4 h-4 text-deep-purple-accent-400"
+											viewBox="0 0 24 24"
+											strokeLinecap="round"
+											strokeLidth="2"
+										>
+											<polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8" />
+											<circle cx="12" cy="12" fill="none" r="11" stroke="currentColor" />
+										</svg>
+									</div>
+									<p className="font-medium text-gray-800">Dos publicaciónes en LinkedIn</p>
+								</li>
+								<li className="flex items-center">
+									<div className="mr-2">
+										<svg
+											className="w-4 h-4 text-deep-purple-accent-400"
+											viewBox="0 0 24 24"
+											strokeLinecap="round"
+											strokeLidth="2"
+										>
+											<polyline fill="none" stroke="currentColor" points="6,12 10,16 18,8" />
+											<circle cx="12" cy="12" fill="none" r="11" stroke="currentColor" />
+										</svg>
+									</div>
+									<p className="font-medium text-gray-800">Dos mensajes en Telegram.</p>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div>
+						<a
+							href="mailto:trabajosremotos@mango.moe"
+							className="inline-flex items-center justify-center w-full h-12 px-6 mb-4 font-medium tracking-wide text-white transition duration-200 bg-gray-800 rounded shadow-md hover:bg-opacity-80		 focus:shadow-outline focus:outline-none brand-bg-color"
+						>
+							Ponte en contacto
+						</a>
+						<p className="text-sm text-gray-600">
+							Para mas información, puedes ponerte en contactarnos en{" "}
+							<a href="mailto:trabajosremotos@mango.moe" className="underline text-indigo-500">
+								trabajosremotos@mango.moe
+							</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
