975010fd3a9363efab61c9a0f05686f55b253af0
Hendy Serrano <hello@hserrano.me>
2021-10-05 19:03:03 +0200

    CRITICAL: updated nextjs and sentry

--- next.config.js
+++ next.config.js
@@ -1,22 +1,21 @@
 // next.config.js
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
-const withCSS = require('@zeit/next-css');
-const withSass = require('@zeit/next-sass');
-const withImages = require('next-images');
-const withOffline = require('next-offline');
-const withSourceMaps = require('@zeit/next-source-maps');
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const withCSS = require("@zeit/next-css");
+const withSass = require("@zeit/next-sass");
+const withImages = require("next-images");
+const withOffline = require("next-offline");
+const withSourceMaps = require("@zeit/next-source-maps");
+const { withSentryConfig } = require("@sentry/nextjs");
 
-require('dotenv').config()
+require("dotenv").config();
 
 function HACK_removeMinimizeOptionFromCssLoaders(config) {
-	console.warn(
-		'HACK: Removing `minimize` option from `css-loader` entries in Webpack config',
-	);
-	config.module.rules.forEach(rule => {
+	console.warn("HACK: Removing `minimize` option from `css-loader` entries in Webpack config");
+	config.module.rules.forEach((rule) => {
 		if (Array.isArray(rule.use)) {
-			rule.use.forEach(u => {
-				if (u.loader === 'css-loader' && u.options) {
+			rule.use.forEach((u) => {
+				if (u.loader === "css-loader" && u.options) {
 					delete u.options.minimize;
 				}
 			});
@@ -24,24 +23,34 @@ function HACK_removeMinimizeOptionFromCssLoaders(config) {
 	});
 }
 
-module.exports = withSourceMaps(withImages(withOffline(
-		withSass(withCSS(
-			{
-				webpack(config) {
-					HACK_removeMinimizeOptionFromCssLoaders(config);
-					config.plugins = [
-						...config.plugins,
-						new Dotenv({
-							path: path.join(__dirname, '.env'),
-							systemvars: true
-						})
-					]
-					config.watchOptions = {
-						poll: 1000,
-						aggregateTimeout: 300,
-					}
-					return config;
-				},
-			}
-		))
-	)))
+const moduleExports = withSourceMaps(
+	withImages(
+		withOffline(
+			withSass(
+				withCSS({
+					webpack(config) {
+						HACK_removeMinimizeOptionFromCssLoaders(config);
+						config.plugins = [
+							...config.plugins,
+							new Dotenv({
+								path: path.join(__dirname, ".env"),
+								systemvars: true,
+							}),
+						];
+						config.watchOptions = {
+							poll: 1000,
+							aggregateTimeout: 300,
+						};
+						return config;
+					},
+				})
+			)
+		)
+	)
+);
+
+const SentryWebpackPluginOptions = {
+	silent: true,
+};
+
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,10 @@ import App from "next/app";
 import Router from "next/router";
 import NProgress from "nprogress";
 import { CookiesProvider } from "react-cookie";
-import * as Sentry from "@sentry/browser";
 
 import "../static/css/style.scss";
 
 import Layout from "../components/Layout";
-
-Sentry.init({ dsn: "https://ac20f57b88b54d819e761b537545aa93@o376610.ingest.sentry.io/5197584" });
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
@@ -35,14 +32,6 @@ class MyApp extends App {
 	}
 
 	componentDidCatch(error, errorInfo) {
-		Sentry.withScope((scope) => {
-			Object.keys(errorInfo).forEach((key) => {
-				scope.setExtra(key, errorInfo[key]);
-			});
-
-			Sentry.captureException(error);
-		});
-
 		super.componentDidCatch(error, errorInfo);
 	}
 
